1352f2fcfc0ff84c05189790edffe0ddc335439d
sanza40 <mariacruz.sanz@murciaeduca.es>
2016-03-06 21:30:13 +0100

    Update EjED.java

--- EjED/src/ejed/EjED.java
+++ EjED/src/ejed/EjED.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejed;
 
 /**
@@ -15,7 +10,7 @@ public class EjED {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int i,j, cont=0, num;//cont no se utiliza
+        int i,j, num;
 
 	num=ES.leeNº("Escribe un numero"); //Leído por la clase ES
 	for(i=0;i<num;i++){
